7a2b59bc3e873fda453f365fe052da05c8f94542
themegat <tshepomotsoeneng0@gmail.com>
2025-04-20 00:14:09 +0200

    Refactor and improvements

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,4 +1,6 @@
 const Generator = require("yeoman-generator");
+const _ = require("lodash");
+const fs = require("fs");
 
 module.exports = class extends Generator {
   initializing() {}
@@ -13,25 +15,42 @@ module.exports = class extends Generator {
     }).then((answers) => {
       // Store the extensionName for use in the writing phase
       this.extensionConfig = answers.extensionConfig;
+      this.extensionConfig.installDependencies = false;
     });
   }
 
   writing() {
-    // Access the extensionName stored in the prompting phase
-    const extensionName = this.extensionConfig.name;
-    const displayName = this.extensionConfig.displayName;
+    return this.prompt([
+      {
+        type: "input",
+        name: "displayName",
+        message: "What's the display name of your extension?",
+        default: this.extensionConfig.displayName,
+      },
+      {
+        type: "confirm",
+        name: "runNpmInstall",
+        message: "Would you like to run npm install?",
+        default: true,
+      },
+    ]).then((answers) => {
+      this.extensionConfig.displayName = answers.displayName;
+      this.env.options.skipInstall = !answers.runNpmInstall;
 
-    // Copy only the .docker and .github directories and their contents
-    const directoriesToCopy = [".docker", ".github", ".scripts", ".actrc"];
+      // Access the extensionName stored in the prompting phase
+      const extensionName = this.extensionConfig.name;
+      const displayName = this.extensionConfig.displayName;
 
-    directoriesToCopy.forEach((dir) => {
-      this.fs.copy(
-        this.templatePath(dir),
-        this.destinationPath(`${extensionName}/${dir}`)
-      );
-    });
+      // Copy the .actrc file and .docker .scripts directories and their contents
+      const directoriesToCopy = [".docker", ".github", ".scripts", ".actrc"];
+      directoriesToCopy.forEach((dir) => {
+        this.fs.copy(
+          this.templatePath(dir),
+          this.destinationPath(extensionName, dir)
+        );
+      });
 
-      // Replace {displayName} in workflows
+      // Copy the GitHub Actions workflow files and overwrite the placeholders
       const workflows = [
         ".github/workflows/test.yml",
         ".github/workflows/publish.yml",
@@ -44,24 +63,33 @@ module.exports = class extends Generator {
           this.fs.write(workflowPath, content);
         }
       });
+    });
   }
 
   install() {
     const extensionName = this.extensionConfig.name;
+    const displayName = this.extensionConfig.displayName;
 
     // Update the package.json
-    const packageJsonPath = this.destinationPath(extensionName, "package.json");
-    const packageJson = this.fs.readJSON(packageJsonPath);
-    packageJson.scripts = {
-      ...packageJson.scripts,
-      "test:coverage":
-        "vscode-test --label 0 --coverage --coverage-reporter json-summary --coverage-output coverage/coverage-summary",
-      "docker:build-test":
-        "docker build -f .docker/test/dockerfile --pull=false -t act-test-ubuntu-node .",
-      "act:test":
-        'act --pull=false --workflows ".github/workflows/test.yml" --secret-file "" --var-file "" --input-file "" --eventpath ""',
-      "vscode:download": "node ./scripts/vscode_downloader.js",
+    const pkgJson = {
+      displayName: displayName,
+      scripts: {
+        "test:coverage":
+          "vscode-test --label 0 --coverage --coverage-reporter json-summary --coverage-output coverage/coverage-summary",
+        "docker:build-test":
+          "docker build -f .docker/test/dockerfile --pull=false -t act-test-ubuntu-node .",
+        "act:test":
+          'act --pull=false --workflows ".github/workflows/test.yml" --secret-file "" --var-file "" --input-file "" --eventpath ""',
+        "vscode:download": "node ./.scripts/vscode_downloader.js",
+      },
     };
-    this.fs.writeJSON(packageJsonPath, packageJson);
+    const packageJsonContent = this.fs.readJSON(
+      this.destinationPath(extensionName, "package.json")
+    );
+    const newPackageJsonContent = _.merge(packageJsonContent, pkgJson);
+    fs.writeFileSync(
+      this.destinationPath(extensionName, "package.json"),
+      JSON.stringify(newPackageJsonContent, null, 4)
+    );
   }
 };
